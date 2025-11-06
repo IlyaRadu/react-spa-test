5bc70a975a5eeb3975ba25c49f7c3d0c09f7934f
Anastasiia <anastasiia@MacBookPro.home>
2025-11-06 17:28:23 +0100

    fix deploy

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = createRoot(document.getElementById('root')!);
 
+// basename берём из PUBLIC_URL (CRA) или указываем явно '/react-spa-test'
 root.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>
+  <BrowserRouter basename={process.env.PUBLIC_URL || '/react-spa-test'}>
+    <App />
+  </BrowserRouter>
 );
